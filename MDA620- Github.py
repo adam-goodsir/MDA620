81a782560b78497bcf401c34f938ede2219cce4b
adam-goodsir <113154521+adam-goodsir@users.noreply.github.com>
2022-09-15 20:29:56 -0400

    Add files via upload

---
+++ MDA620- Github.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import fib 
+
+
+# In[5]:
+
+
+x = 5
+y = 6
+print(x*y)
+
+
+# In[6]:
+
+
+7 % 5
+
+
+# In[7]:
+
+
+int(3.5)
+
+
+# In[9]:
+
+
+import math
+math.pi
+
